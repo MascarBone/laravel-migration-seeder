2644c8b22fca904348639ecd4aabefe938085eca
MascarBone <simone.bonelli@gmail.com>
2021-11-12 15:46:26 +0100

    model and migrations

---
+++ app/Models/Journey.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class Journey extends Model
+{
+    use SoftDeletes;
+
+    private $fillable = ['country','region','city','location','date_start','duration'.'placement','service','description','available','price'];
+}
---
+++ database/migrations/2021_11_12_140845_create_journeys_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateJourneysTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('journeys', function (Blueprint $table) {
+            $table->id();
+            $table->string('country', 50);
+            $table->string('region', 50);
+            $table->string('city', 50);
+            $table->string('location',100);
+            $table->date('date_start');
+            $table->smallInteger('duration');
+            $table->string('placemnet', 255);
+            $table->string('service',255);
+            $table->text('description');
+            $table->boolean('available')->nullable();
+            $table->float('price',8,6)->unsigned();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('journeys');
+    }
+}
